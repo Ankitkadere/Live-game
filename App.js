bfbf200bb4e7dd964e82dd802a35831d073c5f67
ankit <ankitkadere2904@gmail.comgit>
2025-11-28 12:26:52 +0530

    updated

--- App.js
+++ App.js
@@ -219,8 +219,8 @@ function loadData() {
     <div class="bg-white px-5 py-4 rounded-2xl shadow">
       <div class="justify-between">
         <div class="flex justify-between">
-         <b class="text-3x1 text-black text-extrabold ">${r.Id}</b>
-         <b class="text-gray-400"> ${formatDate(r.Date)} → ${formatTime(r.Time)} </b>
+         <b class="text-3x1 text-black text-extrabold  text-left">${r.Id}</b>
+         <b class="text-gray-400  text-right"> ${formatDate(r.Date)} → ${formatTime(r.Time)} </b>
         </div>
         <div class="bg-blue-50 text-blue-900 justify-between flex px-3 py-1 mt-2 rounded-md text-lg font-bold">
           <b>${r.Jodi}</b>
